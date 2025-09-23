d62fcad4d9536700a6d111645eef18dd22a03564
Deil <yar020306@gmail.com>
2025-09-23 13:55:28 +0700

    Два примера цикла while

--- Pr006.java
+++ Pr006.java
@@ -60,6 +60,139 @@ class Pr006 {
 				//Все оставшиеся варианты выведут значение, указанное ниже
 				System.out.println("Значение 1 не равно ни одному значение от 1 до 4");	
 		}	
-	
-	}	
-}	
+			//Оператор switch
+			System.out.println("\n Демонстрация работы оператора switch()");
+			for (i=0; i<10; i++)
+				switch(i){
+					case 0:
+						System.out.println("i = 0");
+						break;
+					case 1: 
+						System.out.println("i = 1");
+						break;
+					case 2:
+					        System.out.println("i = 2");
+						break;	
+					case 3:
+						System.out.println("i = 3");
+						break;
+					case 4:
+						System.out.println("i = 4");
+						break;
+					default:
+						System.out.println("i >= 5");
+				}
+			//Вложенные операторы switch
+			
+			char ch2 = 'R';
+			char ch1 = 'A';
+			switch(ch2) {
+				case 'A': 
+					System.out.println("Значение А внешного оператора switch");
+				switch(ch1){
+					case 'A': 
+						System.out.println("Значение А внутреннего оператора switch");
+					break;					
+				}
+
+				break;
+				case 'R': 
+					System.out.println("Значение R внешного оператора switch");
+
+				switch(ch1){
+					case 'A': 
+						System.out.println("Значение А внутреннего оператора switch");
+					break;
+				}
+				break;
+
+				default:
+					System.out.println("Значение по умолчанию для внешнего switch()");	
+			
+			}
+
+			//оператор цикла for
+
+			System.out.println();
+			double num, sroot, rerr;
+			for (num = 1.0; num < 100.0; num++) {
+				sroot = Math.sqrt(num);
+				System.out.println("Квадратный корень из " + num + " равен " + sroot);
+
+				//Вычисление ошибки округления
+				rerr = num - (sroot*sroot);
+				System.out.println("Ошибка округления: " + rerr);
+				System.out.println();
+			}
+			
+			System.out.println();
+			int x;
+			for (x = 100; x > -100; x -= 5)
+				System.out.println(x);
+
+			System.out.println("\nПример цикла, тело которого не выполняется ни разу");
+			for (x = 100; x > 100; x -= 5)
+				 System.out.println(x);
+			System.out.println("После цикла for");
+
+			System.out.println();
+
+			for (i = 0, j = 10; i < j; i++, j--)
+				System.out.println("i = " + i + " j = " + j);
+			//Чтение символов с клавиатуры внутри цикла for
+			System.out.print("\nВведите \'S\' для остановки цикла чтения с клавиатуры ");
+			for(i=0; (char) System.in.read() != 'S'; i++) {
+				System.out.println("Итерация № " + i);
+				temp = (char) System.in.read();
+			}
+			System.out.println();
+
+			//Пропуск отдельных частей в определении цикла for
+			System.out.println();
+			for (i = 0; i < 10;) {
+				System.out.println("Итерация №" + i);
+				i++;
+			}
+			//Бесконечный цикл
+			System.out.println();
+			for(;;) {
+				System.out.println("Итерация");
+				System.out.println("Для остановки цикла введите \'q\'");
+				temp = (char) System.in.read();
+				if (temp == 'q')
+					break;
+			}
+
+			//Циклы без тела
+			System.out.println();
+			int sum = 0;
+			for (i = 1; i <= 5; sum += i++);
+			System.out.println("Значение sum: " + sum);
+
+			//Объявление параметра цикла внутри цикла
+			
+			for(int ii = 1; ii <= 10; sum += ii++);
+			System.out.println("Значение sum " + sum);
+		//	System.out.println("Значение ii:" + ii);
+		
+			//Цикл while()
+			char ch3 = 'a';
+			while(ch3 <= 'z') {
+				System.out.print(ch3 + " ");
+				ch3++;
+			}
+			System.out.println();
+			System.out.println();
+			int e, result;
+
+			for(int ii = 0; ii < 10; ii++) {
+				result = 1;
+				e = ii;
+				while(e>0) {
+					result *= 2;
+					e--;
+				}
+				System.out.println("2 в степени " + ii + " равно " + result);
+			}
+	}
+}

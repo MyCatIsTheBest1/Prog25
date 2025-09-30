b838f4d49aa2a401afa7615b3dbf4f32da36bda7
deil <yar020306@gmail.com>
2025-09-30 19:29:54 +0700

    Добавили в файл help циклы

--- HELP.java
+++ HELP.java
@@ -1,12 +1,23 @@
 class Help {
 	public static void main(String args[])
 	throws java.io.IOException {
-		char choice;
-		System.out.println("Справочная система");
-		System.out.println(" 1. if");
-		System.out.println(" 2. switch");
-		System.out.print("Выберите нужно число: ");
-		choice = (char) System.in.read();
+		char choice, temp;
+		
+		do { 
+		
+			System.out.println("Справочная система");
+			System.out.println(" 1. if");
+			System.out.println(" 2. switch");
+			System.out.println(" 3. for");
+			System.out.println(" 4. while");
+			System.out.println(" 5. do-while");
+			
+			System.out.print("Выберите нужно число: ");
+			choice = (char) System.in.read();
+			do {
+				temp = (char) System.in.read();
+			} while (temp != '\n');
+		} while (choice < '1' | choice > '5');	
 
 		System.out.println("\n");
 
@@ -19,14 +30,27 @@ class Help {
 			case '2':
 				System.out.println("Оператор switch:\n");
 				System.out.println("switch(выражение) {");
-				System.out.println(" case кконстанта:");
+				System.out.println(" case константа:");
 				System.out.println(" последовательность операторов");
 				System.out.println(" break");
 				System.out.println(" //...");
 				System.out.println("}");
 				break;
-			default:
-				System.out.println("Неверный выбор");
+			case '3':
+				System.out.println("Оператор for:\n");
+                                System.out.println("for(инициализация; условие; итерация)");
+				System.out.println(" оператор");
+				break;
+			case '4':
+				System.out.println("Оператор while:\n");
+                                System.out.println("while(условие) оператор;");
+                                break;
+			case '5':
+                                System.out.println("Оператор do-while:\n");
+                                System.out.println("do(");
+                                System.out.println(" оператор;");
+				System.out.println(") while (условие);");
+                                break;
 		}
 	}
 }

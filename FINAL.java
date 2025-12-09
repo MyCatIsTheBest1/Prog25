fd9c4c88b41620667412cadd042def994159cdd0
Deil <yar020306@gmail.com>
2025-12-09 16:46:07 +0700

    Демонстрация final в программе FinalDemo

---
+++ FINAL.java
@@ -0,0 +1,37 @@
+class A {
+	void meth() {
+		System.out.println("Метод meth() класса А");
+	}
+	final void meth2() {
+		System.out.println("Метод meth2 не допускает переопределения в подклассах");
+	}
+}
+class B extends A {
+	void meth() {
+		System.out.println("Метод meth() класса B");
+	}
+}
+final class C {
+	int i=10;
+}
+class D extends A {
+	int j = 20;
+}
+class ErrorMsg {
+	final int OUTERR = 0;
+	final int INERR = 1;
+	final int DISKERR = 2;
+	final int INDEXERR = 3;
+	String[] msgs = {
+		"Ошибка вывода",
+		"Ошибка ввода",
+		"Переполнение диска",
+		"Выход индекса за границы массива"
+	};
+	String getErrorMsg(int i) {
+		if(i >= 0 & i < msgs.length)
+			return msgs[i];
+		else
+			return "Неизвестная ошибка";
+	}
+}
